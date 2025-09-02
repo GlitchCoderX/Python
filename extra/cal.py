e97c4fff6e7520bdf457553a280cee72b7bf90c5
GlitchCoderX <nahidhossain817064@gmail.com>
2025-09-02 19:35:31 +0600

    Update cal.py

--- extra/cal.py
+++ extra/cal.py
@@ -1,21 +1,21 @@
-print("Calculator. availalble + - * /")
+print("Calculator. Available: + - * /")
 
 num1 = int(input("Enter 1st number: "))
-action= input("what action you want to do?")
+action = input("What action do you want to do? ")
 num2 = int(input("Enter 2nd number: "))
 
-print("answer is : ")
-if action=="+":
-    print(num1+num2)
-elif action == "-":
-    print(num1-num2)
-elif action =="*":
-    print(num1*num2)
-elif action == "/":
-    if num1 == 0:
-        print("Unexpected input!!!")
-    if num2 == 0:
-        print("Unexpected input!!!")
-    else : print(num1/num2)
-else:  print("Unexpected input!!!")
+print("Answer is: ", end="")
 
+if action == "+":
+    print(num1 + num2)
+elif action == "-":
+    print(num1 - num2)
+elif action == "*":
+    print(num1 * num2)
+elif action == "/":
+    if num2 == 0:
+        print("Unexpected input!!! (cannot divide by zero)")
+    else:
+        print(num1 / num2)
+else:
+    print("Unexpected input!!!")
