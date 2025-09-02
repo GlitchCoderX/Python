67535a4756bf117087d4a59ab48d2aacb75f1818
GlitchCoderX <nahidhossain817064@gmail.com>
2025-09-02 19:22:38 +0600

    Add files via upload

---
+++ day_2/class_2.py
@@ -0,0 +1,62 @@
+#oparetor
+#assingment operators
+a = 5
+a = 10 
+print(a) #truned a=5 into a=10
+
+#adition operator
+b = 6
+#b = 6 + 2 or
+# b = b+2 or
+b += 2
+print(b)
+
+#substarction
+c = 4
+#c = 4+ 8
+#c = c + 8
+c += 8
+print(c)
+
+#multipliacitaion
+d = 3
+#d = 3 * 4
+#d = d * 4
+d *= 4
+print(d)
+
+#divition
+e = 10
+# e = 10 /2
+# e = e / 2
+e /= 2
+print(e)
+
+#modulus
+f = 15
+# use your own iq
+f %= 4
+print(f)
+
+#floor division
+g = 13
+# use your own iq.
+g //= 3
+print(g)
+print("div---")
+
+#comparison operator
+print(c == d)
+print( d != b)
+print( g > f)
+print( a < b)
+print( c <= d)
+print( f <= c)
+print("div---")
+#boolean
+print(bool(a == b))
+print("div---")
+#boolean oparetors
+print(a >= b and c == d)
+print(a <= b or a == b)
+print(not(a <= b or a == b))
